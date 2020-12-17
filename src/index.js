2a9a9250df0f4d1172c78625a57db61b8ea008a7
RasKids <RasKids@macs-mbp-2.lan>
2020-12-17 08:35:16 -0700

    init

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log("you're kinda cute 😩🥺")
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
