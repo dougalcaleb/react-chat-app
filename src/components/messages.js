7d38ca907262c216fd7414c23110bead7d87da8e
dougalcaleb <dougalcaleb@gmail.com>
2021-01-05 08:16:01 -0700

    Changed to a class, message sending methods

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,30 +1,83 @@
 import React from "react";
 import '../styles/index.css';
 import Message from './message';
+import { sendMessage, messageList } from "../services/message-handler";
+import { store } from "../services/data-handler";
 
-const Messages = () => {
-    return(
-        <div className="messages">
+class Messages extends React.Component {
+   constructor(props) {
+      super(props);
+
+      // this.state = {
+      //    messsages: []
+      // };
+   }
+
+   componentDidMount = () => {
+      store.subscribe(() => {
+         console.log("Store was updated. Outputting new messages list:");
+         console.log(store.getState().messages);
+         this.setState({
+            messages: store.getState().messages
+         });
+      });
+   }
+   
+   handleMessageSend = () => {
+      sendMessage(document.querySelector(".pendingMessage").value);
+      document.querySelector(".pendingMessage").value = "";
+   }
+   handleKeyDown = (e) => {
+      switch (e.key) {
+         case "Enter":
+            this.handleMessageSend();
+            break;
+      }
+   }
+   addNewMessage = (msg) => {
+      console.log("New message, poggers");
+   }
+   render() {
+      store.subscribe(this.addNewMessage);
+
+
+      console.log("Message list is:");
+      // console.log(this.state.messages);
+      const messages = store.getState().messages.map((msg) => {
+         <Message
+            name={msg.displayName}
+            message={msg.text}
+            time={msg.timestamp}
+            picture={msg.pic}
+         />
+      });
+      return (
+         <div className="messages">
             <div className="chat">
-                <div className="message recieved">What's poppin</div>
-                <div className="message sent">Brand new whip just hopped in</div>
-                <div className="message recieved">I got options</div>
-                <div className="message sent">I could pass that snitch like Stockton</div>
-                <div className="message recieved">Just joshin</div>
-                <div className="message sent">Ima spend my holiday locked in</div>
-                <div className="message recieved">My body got rid of them toxins</div>
-                <div className="message sent">Sportscenter, top ten</div>
-                <Message
-                    name="Anders"
-                    message="EBICCOO"
-                />
+               {/* <div className="message recieved">What's poppin</div>
+               <div className="message sent">Brand new whip just hopped in</div>
+               <div className="message recieved">I got options</div>
+               <div className="message sent">I could pass that snitch like Stockton</div>
+               <div className="message recieved">Just joshin</div>
+               <div className="message sent">Ima spend my holiday locked in</div>
+               <div className="message recieved">My body got rid of them toxins</div>
+               <div className="message sent">Sportscenter, top ten</div> */}
+               {/* <Message
+                  name="Anders"
+                  message="EBICCOO"
+                  className = "message recieved"
+               /> */}
+
+               {messages}
+
             </div>
             <div className="sendMessage">
-                <input type="text" placeholder="Message epic channel"/>
-                <button>Send</button>
+               <input type="text" placeholder="Message epic channel" className="pendingMessage" onKeyPress={this.handleKeyDown}/>
+               <button onClick={this.handleMessageSend}>Send</button>
             </div>
-        </div>
-    )
+         </div>
+      )
+   }
 }
 
-export default Messages
+export default Messages;
