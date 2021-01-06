ac1002b81f3afc9b4553887c9cece9a280a6259a
RasKids <RasKids@macs-mbp-2.lan>
2021-01-06 08:31:27 -0700

    we now scroll to the bottom now

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,88 +1,88 @@
 import React from "react";
-import '../styles/index.css';
-import Message from './message';
+import "../styles/index.css";
+import Message from "./message";
 import { sendMessage, messageList } from "../services/message-handler";
 import { store } from "../services/data-handler";
 
 class Messages extends React.Component {
-   constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      // this.state = {
-      //    messsages: []
-      // };
-      this.unsub = null;
-   }
-//    updateScroll(){
-//     var element = document.getElementById("yourDivID");
-//     element.scrollTop = element.scrollHeight;
-// }
-   componentDidMount = () => {
-      this.unsub = store.subscribe(this.handleNewMessage);
-   }
+    // this.state = {
+    //    messsages: []
+    // };
+    this.unsub = null;
+  }
+  //    updateScroll(){
+  //     var element = document.getElementById("yourDivID");
+  //     element.scrollTop = element.scrollHeight;
+  // }
+  componentDidMount = () => {
+    this.unsub = store.subscribe(this.handleNewMessage);
+  };
 
-   componentWillUnmount = () => {
-      this.unsub();
-   }
+  componentWillUnmount = () => {
+    this.unsub();
+  };
 
-   handleNewMessage = () => {
-      console.log("Store was updated. Outputting new messages list:");
-      this.setState({
-         messages: store.getState().messages,
-      });
-   }
-   
-   handleMessageSend = () => {
-      sendMessage(document.querySelector(".pendingMessage").value, "sent");
-      document.querySelector(".pendingMessage").value = "";
+  handleNewMessage = () => {
+    console.log("Store was updated. Outputting new messages list:");
+    this.setState({
+      messages: store.getState().messages,
+    });
+  };
+
+  handleMessageSend = () => {
+    sendMessage(document.querySelector(".pendingMessage").value, "sent");
+    document.querySelector(".pendingMessage").value = "";
     //   this.updateScroll()
-   }
-   handleKeyDown = (e) => {
-      switch (e.key) {
-         case "Enter":
-            this.handleMessageSend();
-            break;
-      }
-   }
-   addNewMessage = (msg) => {
-      console.log("New message, poggers");
-   }
-   render() {
-      // store.subscribe(this.addNewMessage);
-
-
-      // console.log("Message list is:");
-      // console.log(this.state.messages);
-      const messages = store.getState().messages.map((msg) => {
-         return (<Message
-            name={msg.displayName}
-            message={msg.text}
-            time={msg.timestamp}
-            pic={msg.pic}
-            key={msg.timestamp}
-            clas={msg.who}
-         />)
-      });
+  };
+  handleKeyDown = (e) => {
+    switch (e.key) {
+      case "Enter":
+        this.handleMessageSend();
+        break;
+    }
+  };
+  addNewMessage = (msg) => {
+    console.log("New message, poggers");
+  };
+  render() {
+    const messages = store.getState().messages.map((msg) => {
       return (
-         <div className="messages">
-            <div className="chat">
-               <Message
-                  name="Anders"
-                  message="EBICCOO"
-                  clas="messageWrap sent"
-                  time={1609861938035}
-               />
+        <Message
+          name={msg.displayName}
+          message={msg.text}
+          time={msg.timestamp}
+          pic={msg.pic}
+          key={msg.timestamp}
+          clas={msg.who}
+        />
+      );
+    });
+    return (
+      <div className="messages">
+        <div className="chat">
+          <Message
+            name="Anders"
+            message="EBICCOO"
+            clas="messageWrap sent"
+            time={1609861938035}
+          />
 
-               {messages}
-
-            </div>
-            <div className="sendMessage">
-               <input type="text" placeholder="Message epic channel" className="pendingMessage" onKeyPress={this.handleKeyDown}/>
-               <button onClick={this.handleMessageSend}>Send</button>
-            </div>
-         </div>
-      )
-   }
+          {messages}
+        </div>
+        <div className="sendMessage">
+          <input
+            type="text"
+            placeholder="Message epic channel"
+            className="pendingMessage"
+            onKeyPress={this.handleKeyDown}
+          />
+          <button onClick={this.handleMessageSend}>Send</button>
+        </div>
+      </div>
+    );
+  }
 }
 export default Messages;
-
