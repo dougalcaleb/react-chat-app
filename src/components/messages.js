38f2850cbe9d9bb851abaa4821e867dfafa1e568
RasKids <RasKids@macs-MacBook-Pro-2.local>
2021-01-07 08:37:35 -0700

    sent epicly

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,66 +1,81 @@
 import React from "react";
 import "../styles/index.css";
 import Message from "./message";
-import {sendMessage, messageList} from "../services/message-handler";
-import {store} from "../services/data-handler";
-import {v4 as uuidv4} from "uuid";
+import { sendMessage, messageList } from "../services/message-handler";
+import { store } from "../services/data-handler";
+import { v4 as uuidv4 } from "uuid";
+import {userData} from "../App"
 
 const messagesToShow = 75;
 
 class Messages extends React.Component {
-	constructor(props) {
-		super(props);
-		this.unsub = null;
-	}
-	//    updateScroll(){
-	//     var element = document.getElementById("yourDivID");
-	//     element.scrollTop = element.scrollHeight;
-	// }
-	componentDidMount = () => {
-		this.unsub = store.subscribe(this.handleNewMessage);
-	};
+  constructor(props) {
+    super(props);
+    this.unsub = null;
+  }
+  componentDidMount = () => {
+    this.unsub = store.subscribe(this.handleNewMessage);
+  };
 
-	componentWillUnmount = () => {
-		this.unsub();
-	};
+  componentWillUnmount = () => {
+    this.unsub();
+  };
 
-	handleNewMessage = () => {
-      console.log("Store was updated. Outputting new messages list:");
-      console.log(store.getState().messages);
-		this.setState({
-			messages: store.getState().messages,
-		});
-	};
+  handleNewMessage = () => {
+    console.log("Store was updated. Outputting new messages list:");
+    console.log(store.getState().messages);
+    this.setState({
+      messages: store.getState().messages,
+    });
+  };
 
-	handleMessageSend = () => {
-		sendMessage(document.querySelector(".pendingMessage").value, "sent");
-		document.querySelector(".pendingMessage").value = "";
-	};
-	handleKeyDown = (e) => {
-		switch (e.key) {
-			case "Enter":
-				this.handleMessageSend();
-				break;
+  handleMessageSend = () => {
+    sendMessage(document.querySelector(".pendingMessage").value);
+    document.querySelector(".pendingMessage").value = "";
+  };
+  handleKeyDown = (e) => {
+    switch (e.key) {
+      case "Enter":
+        this.handleMessageSend();
+        break;
+    }
+  };
+  render() {
+    const msgs = store.getState().messages;
+    while (msgs.length > messagesToShow) {
+      msgs.shift();
+    }
+    const messages = msgs.map((msg) => {
+		let sent;
+		if (userData.uuid == msg.uuid) {
+			sent = "sent";
 		}
-	};
-	render() {
-		const msgs = store.getState().messages;
-      while (msgs.length > messagesToShow) {
-         msgs.shift();
-      }
-		const messages = msgs.map((msg) => {
-			return <Message name={msg.displayName} message={msg.text} time={msg.timestamp} pic={msg.pic} key={uuidv4()} clas={msg.who} />;
-		});
+      return (
+        <Message
+          name={msg.displayName}
+          message={msg.text}
+          time={msg.timestamp}
+          pic={msg.pic}
+		  key={uuidv4()}
+		  clas={sent}
+        />
+      );
+    });
 
-		return (
-			<div className="messages">
-				<div className="chat">{messages}</div>
-				<div className="sendMessage">
-					<input type="text" placeholder="Message epic channel" className="pendingMessage" onKeyPress={this.handleKeyDown} />
-					<button onClick={this.handleMessageSend}>Send</button>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="messages">
+        <div className="chat">{messages}</div>
+        <div className="sendMessage">
+          <input
+            type="text"
+            placeholder="Message epic channel"
+            className="pendingMessage"
+            onKeyPress={this.handleKeyDown}
+          />
+          <button onClick={this.handleMessageSend}>Send</button>
+        </div>
+      </div>
+    );
+  }
 }
 export default Messages;
